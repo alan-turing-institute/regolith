da1beb1f7242a5152c650b3b5e1a266748ddd0b7
Oliver Strickson <ostrickson@turing.ac.uk>
2020-12-04 10:14:02 +0000

    Use whatnow to get Forecast schedule; convert to monthly assignments for grid output

--- finance.rkt
+++ finance.rkt
@@ -9,11 +9,14 @@
 ;; nocell and whatnow as dependencies
 
 
-(require (prefix-in gregor: gregor)
+(require (rename-in gregor
+                    [date gregor:date]
+                    [date? gregor:date?])
+         whatnow/forecast
+         whatnow/schedule
          "../grid/grid.rkt"
          "../ods/ods.rkt")
 
-;; (require whatnow/forecast)
 
 ;; ----------------------------------------
 ;; Forecast monthly allocation data
@@ -70,7 +73,8 @@
 ;; Allocations as a 'wide' table
 (struct/contract allocation*
   allocation-key
-  ([month-fraction (hash/c gregor:date? real?)])
+  ([month-fraction (hash/c gregor:date? real?
+                           #:flat? #t)])
   #:transparent)
 
 
@@ -88,6 +92,158 @@
                                       (allocation-fraction a)))
                          group))))))
 
+
+;; ----------------------------------------
+;; Date interval helpers
+
+;; Represents the half-open date interval [start, end).
+;; (`start > end` is allowed, and represents an empty interval)
+(struct/contract date-interval ([start gregor:date?]
+                                [end gregor:date?])
+                 #:transparent)
+
+(define (date-min a b) (if (date<? a b) a b))
+(define (date-max a b) (if (date>=? a b) a b))
+
+;; date-interval-intersect : date-interval? date-interval? -> date-interval?
+(define (date-interval-intersect I J)
+  (let ([start (date-max (date-interval-start I) (date-interval-start J))]
+        [end* (date-min (date-interval-end I) (date-interval-end J))])
+    (date-interval start (date-max start end*))))
+
+;; date-interval-days : date-interval? -> exact-integer?
+(define (date-interval-days I)
+  (days-between (date-interval-start I) (date-interval-end I)))
+
+;; workday? : gregor:date? -> boolean?
+(define (workday? d) (not (or (saturday? d) (sunday? d))))
+
+;; in-date-interval : date-interval? -> stream?
+(define (in-date-interval I)
+  (let ([start (date-interval-start I)]
+        [end (date-interval-end I)])
+    (if (date>=? start end)
+        empty-stream
+        (stream-cons
+         start
+         (in-date-interval (date-interval (+days start 1) end))))))
+
+;; The number of working days in the given date interval
+;;
+;; net-workdays : date-interval? -> exact-nonnegative-integer?
+(define (net-workdays I)
+  (let-values ([(start) (date-interval-start I)]
+               [(w d) (quotient/remainder (date-interval-days I) 7)])
+    ;; five working days per full week, plus remaining days determined by workday?
+    (+ (* 5 w)
+       (sequence-count workday?
+                       (in-date-interval
+                        (date-interval start (+days start d)))))))
+
+
+;; ----------------------------------------
+;; Retreive data from Forecast via whatnow, returned as 'allocation' objects
+
+;; Conversion factor for Forecast assignments (28800 seconds in an 8 hour workday)
+(define secs/workday.fte-fraction 28800)
+
+;; Does this string have the correct form for a project code?
+;;
+;; project-code? : string? -> boolean?
+(define (project-code? t)
+  (regexp-match? #px"^R-[[:alnum:]]+-[[:alnum:]]+$" t))
+
+;; Return mappings (as hash tables) from ids to names, for the data
+;; returned from Forecast:
+;;  - person-id => full name 
+;;  - client-id => client-name
+;;  - project-id => (list client-name project-name project-code)
+;; 
+;; schedule-id-name-mappings : schedule? -> (values (hash/c number? string?)
+;;                                                  (hash/c number? string?)
+;;                                                  (hash/c number? string?))
+(define (schedule-id-name-mappings whatnow-schedule)
+  (match-let*
+      ([(schedule people projects programmes assignments) whatnow-schedule]
+       
+       ;; person-id to full name
+       ;; : (hash/c number? string?)
+       [person-id=>name
+        (for/hash ([p people])
+          ;; check for REG permanent RSE/RDS...
+          (values (person-id p)
+                  (string-join (list (person-first-name p)
+                                     (person-last-name p)))))]
+       
+       ;; client-id to client name
+       ;; : (hash/c number? string?)
+       [client-id=>name
+        (for/hash ([p programmes])
+          (values (client-id p)
+                  (client-name p)))]
+       
+       ;; project-id to list of client name, project name and project code
+       ;; : (hash/c number? (listof (or/c string? #f)))       
+       [project-id=>data
+        (for/hash ([p projects])
+          (values (project-id p)
+                  (list (hash-ref client-id=>name (project-client-id p) #f)
+                        (project-name p)
+                        (findf project-code? (project-tags p)))))])
+
+    (values person-id=>name client-id=>name project-id=>data)))
+
+;; Given a (whatnow) assigment, and a month (as a gregor date --
+;; nominally the first day in the month, but could by any date),
+;; return the full-time equivalent fraction of the assignment in that
+;; month.
+;; 
+;; assignment-fte-in-month : assignment? gregor:date? -> inexact-real?
+(define (assignment-fte-in-month a m)
+  (let (;; the input month, as an interval
+        [m* (date-interval m (+months m 1))]
+        ;; the assigment date range as an interval 
+        [a* (date-interval (assignment-start-date a)
+                           ;; Forecast assignments include their end date
+                           (+days (assignment-end-date a) 1))])
+    (exact->inexact
+     (*
+      ;; fractional allocation per workday
+      (/ (assignment-allocation a)
+         secs/workday.fte-fraction)
+      ;; fraction of allocated workdays in the given month
+      (/ (net-workdays (date-interval-intersect a* m*))
+         (net-workdays m*))))))
+
+;; schedule->allocations schedule? (listof gregor:date?) : -> (listof allocation?)
+(define (schedule->allocations whatnow-schedule months)
+  (let-values ([(person-id=>name client-id=>name project-id=>data)
+                (schedule-id-name-mappings whatnow-schedule)]
+               ;;
+               ;; groups of assignments with common person-id and project-id
+               ;;  : (listof (listof assignment?))
+               [(assignment-groups)
+                 (group-by (λ (a) (cons (assignment-person-id a)
+                                        (assignment-project-id a)))
+                           (schedule-assignments whatnow-schedule))])
+    
+    (for/list ([assgns assignment-groups])
+      (match-let
+          ;; use the first assignment in the group for its representative
+          ;; person-id and project-id
+          ([person
+            (hash-ref person-id=>name (assignment-person-id (car assgns))
+                      "UNKNOWN")]
+           [(list client project code)
+            (hash-ref project-id=>data (assignment-project-id (car assgns))
+                      '("UNKNOWN" "UNKNOWN" "UNKNOWN"))])
+        (allocation*
+         client project (or code "") person
+         (for/hash ([m months])
+           (values m
+                   (for/sum ([a assgns]) (assignment-fte-in-month a m)))))))))
+
+
 ;; ----------------------------------------
 ;; Output to nocell grid
 
@@ -101,10 +257,10 @@
              (cell "Project name" '(column-label))
              (cell "Finance code" '(column-label))
              (cell "Person" '(column-label))
-             (map (λ (mon) (cell (gregor:~t mon "MMM yyyy") '(column-label))) dates))
+             (map (λ (mon) (cell (~t mon "MMM yyyy") '(column-label))) dates))
       
       ;; other rows: allocation entries
-      (for/list ([a (allocations-tall->wide example-forecast-records)])
+      (for/list ([a forecast-records])
         (list* (cell (allocation-key-client a) '())
                (cell (allocation-key-project a) '())
                (cell (allocation-key-code a) '())
@@ -113,7 +269,6 @@
                       (cell (hash-ref (allocation*-month-fraction a) mon 0.0)
                             '(output)))
                     dates))))))))
-
 
 (define (write-report forecast-records dates)
   (bytes->file
@@ -124,10 +279,9 @@
     #:type 'fods)
    "example-report-to-finance.fods"))
 
-
 ;; A list of the months comprising the given financial year
 ;;
 ;; fy-months : exact-integer? -> (listof gregor:date?)
-(define (fy-months y) (map (curry gregor:+months (gregor:date 2020 03)) (range 12)))
+(define (fy-months y) (map (curry +months (gregor:date 2020 03)) (range 12)))
 
-(write-report example-forecast-records (fy-months 2020))
+;(write-report example-forecast-records (fy-months 2020))
